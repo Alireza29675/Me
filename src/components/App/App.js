5e8bc0eb94dd44a2af0a694d9c39ad8b726cc3f5
alireza29675 <alireza.sheikholmolouki@gmail.com>
2019-05-22 18:12:18 +0430

    chore(structure): very basic structure was made

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="resume">
+        
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Basic/Basic.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Basic.scss'
+
+export default class Basic extends React.Component {
+
+    render () {
+        return (<header></header>)
+    }
+
+}
